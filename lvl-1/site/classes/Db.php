defeddf125654bca7173de3ba4ee2c16560e4d52
Grigory Alexandrov <alexaandrov@gmail.com>
2016-07-21 10:53:55 +0300

    Created class DB

--- lvl-1/site/classes/Db.php
+++ lvl-1/site/classes/Db.php
@@ -2,8 +2,34 @@
 
 class Db
 {
-    public function __construct()
+    protected $dbh;
+
+    public function __construct($configFile)
     {
-        
+        $this->dbh = new PDO(
+            $configFile['dsn'],
+            $configFile['username'],
+            $configFile['password']
+        );
+    }
+
+    public function execute(string $sql)
+    {
+        if ($this->dbh->prepare($sql)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function query(string $sql, array $data)
+    {
+        if ($this->execute($sql)) {
+            $sth = $this->dbh->prepare($sql, $data);
+            $sth->execute();
+            return $sth->fetchAll();
+        } else {
+            return false;
+        }
     }
 }
